a95c8efae4eabe7cd2117d240aebe78a40ad28db
Simp4Hitagi <denorauten@gmail.com>
2023-04-23 19:37:50 +0200

    Updated cards, footer, proxy, mapping through array and alignment

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Container } from 'react-bootstrap';
 import axios from 'axios';
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-// import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Carousel from 'react-bootstrap/Carousel';
-// import CustomNavbar from './components/Navbar'
 import { Navbar, Form, Container } from 'react-bootstrap';
+// Local
 import CustomFooter from './components/Footer'
 import Loader from './components/Loader'
 import { Home } from './pages/Home';
-// import Carousel from './components/Carousel';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Articles from './pages/Articles';
-
-
 
 function App() {
   const [search, setSearch] = useState('');
   const [items, setItems] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
 
-  // simp4hitagi@gmail.com latest for api, too many requests
+  //denorauten@gmail.com latest for api, too many requests
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -36,7 +30,7 @@ function App() {
           console.log(response);
         } else {
           response = await axios.get(
-            `/search`
+            `/search?q=${search}`
           );
         }
         setIsLoaded(true);
@@ -47,20 +41,17 @@ function App() {
     };
     fetchData();
   }, [search]);
-
-  
+  console.log(items);
 
   return (
     <div className="App">
-    {/* search doesn't unless nav and data are on same page */}
-          {/* Navbar */}
-    {/* <CustomNavbar/> */}
+    {/* ========= Navbar start ========= */}
     <div className="Navbar">
       <Navbar expand="lg" sticky="top" scrolling>
         <Container fluid className='w-100'>
           <Navbar.Brand href="#">
           <img src="https://img.icons8.com/fluency/48/null/news.png" alt='logo'/>          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Toggle aria-controls="navbarScroll"  className='bg-light opacity-20'/>
           <Navbar.Collapse id="navbarScroll">
           <a className="m-3 text-decoration-none" href="#home">Home</a>
           <a className="m-3 text-decoration-none" href="#articles">Articles</a>
@@ -77,88 +68,38 @@ function App() {
         </Container>
       </Navbar>
     </div>
-
+    {/* ========= Navbar end ========= */}
 
     <div className="min-vh-100">
-    {/* using ternary operator to check if data is loaded and if not to display loader */}
-      {isLoaded ? (
-        // empty tag is like a div, but called fragment to display content in browser
-        <>
         <Home/>
- <Container fluid id="articles" className="my-5 p-5 w-100">
-  <Row>
- <h2 className="text-decoration-underline my-2 p-3 display-3">What's happening in the world?</h2>
-        <h2 className='display-5'><b>Apple</b></h2>
-        <Col >
-            {/* =======Carousel======= */}
-            <Carousel fade>`
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://i.postimg.cc/vBpkfzLD/4d-ultra-hd-binary-robot-f7nfr3b9hfyp4j54.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://i.postimg.cc/mgbqYhM3/gUhC7b.webp"
-          alt="Second slide"
-        />
+      {isLoaded ? (
+        <>
+        <Container fluid id="articles" className="my-5 p-5 w-100">
+          <Row>
+            <h2 className="text-decoration-underline my-2 p-3 display-3">What's happening in the world?</h2>
+            {/* Mapping through array of API links from hosted Server side */}
+            {items
+  .filter(item => item.title.includes(search))
+  .map(item => (
+    <Col xs={12} md={6}>
+      <Card id='card' className='w-100 my-3 border border-3 border-dark bg-dark' key={item.urlToImage}>
+        <Card.Img variant="top" className='w-100' id='cardImg' src={item.urlToImage} alt='{item.urlToImage}' fluid/>
+          <h6 className="text-light mx-3 my-2
+          " key={item.author}><b>Author: {item.author}</b></h6>
+        <Card.Body className='text-light opacity-75'>
+          <h3 key={item.title}>{item.title}</h3>
+          <h6 key={item.description}>
+            {item.description}
+          </h6>
+          <br/>
+        </Card.Body>
+          <Button className='w-50 mx-auto my-5' variant="primary" key={item.url}><a target='_blank' rel='noopener noreferrer' className='text-decoration-none' href={item.url}>Go to Article</a></Button>
+      </Card>
+    </Col>
+  ))}
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://i.postimg.cc/YSPsB3P8/e29a9049d142efa67c363057a8a05d05.jpg"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-{/* =======Carousel======= */}
-
-        </Col>
-        <h2 className='display-5'><b>Tesla</b></h2>
-    {items.map(item => (
-      <>
-        <Col xs={6}>
-        {/* ========Card======== */}
-        <Card className='w-75 my-3 border border-3 border-dark' key={item.urlToImage}>
-      <Card.Img variant="top" src={item.urlToImage} alt='{item.urlToImage}' fluid/>
-      <Card.Body className='bg-dark text-light opacity-75'>
-        <h3 key={item.title}>{item.title}</h3>
-        <h6 key={item.description}>
-        {item.description}
-        </h6>
-           <h6 className="text-dark" key={item.author}><b>Author: {item.author}</b></h6>
-        <Button variant="primary" key={item.url}><a target='_blank' rel='noopener noreferrer' class='text-decoration-none' href={item.url}>Go to Article</a></Button>
-      </Card.Body>
-    </Card>
-
-
-        {/* ========Card======== */}
-          
-        </Col>
-
-      </>
-    ))}
-      </Row>
- </Container>
+          </Row>
+        </Container>
         </>
       ) : (
         //Loader
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,10 @@ import {
 } from 'mdb-react-ui-kit';
 
 export default function CustomFooter() {
+  const year = new Date();
+  let currentYear = year.getFullYear()
   return (
-    <MDBFooter className='bg-light text-center text-white'>
+    <MDBFooter className='bg-light text-center text-white w-100'>
       <MDBContainer className='p-4 pb-0'>
         <section className='mb-4'>
 
@@ -35,7 +37,7 @@ export default function CustomFooter() {
       </MDBContainer>
     {/* set year to be dynamic */}
       <div className='text-center p-3 bg-dark' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2020 Copyright: 
+        © {currentYear} Copyright:
         <a className='text-white' href='https://github.com/Simp4Hitagi'>
           Deno Rautenbach
         </a>
