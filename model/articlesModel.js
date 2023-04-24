b40c932e66887caef421a64bd12e4e153264190e
Simp4Hitagi <denorauten@gmail.com>
2023-04-24 15:21:58 +0200

    Added dotenv file for security

--- model/articlesModel.js
+++ model/articlesModel.js
@@ -1,9 +1,13 @@
 const express = require('express')
+require('dotenv').config();
+
 
 const fetch = require("node-fetch");
 
+const API_KEY= process.env.API_KEY;
+
 async function getNews() {
-  const url = "https://newsapi.org/v2/everything?q=tesla&from=2023-03-23&sortBy=publishedAt&apiKey=d1f5c11979d74eb68afe4a4e365c1894";
+  const url = `https://newsapi.org/v2/everything?q=tesla&from=2023-03-23&sortBy=publishedAt&apiKey=${API_KEY}`;
 
   const response = await fetch(url);
   if (!response.ok) {
@@ -15,7 +19,7 @@ async function getNews() {
 
 async function searchNews(q) {
   // search by category
-    const url = `https://newsapi.org/v2/everything?q=${q}&from=2023-03-23&sortBy=publishedAt&apiKey=d1f5c11979d74eb68afe4a4e365c1894`;
+    const url = `https://newsapi.org/v2/everything?q=${q}&from=2023-03-23&sortBy=publishedAt&apiKey=${API_KEY}`;
   
     const response = await fetch(url);
     if (!response.ok) {
