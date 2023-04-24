cafd91a59e4b2968b4014cbce31d19ac93423acf
Simp4Hitagi <denorauten@gmail.com>
2023-04-24 11:21:01 +0200

    removed fliter

--- src/App.js
+++ src/App.js
@@ -82,24 +82,24 @@ function App() {
             <h2 className="text-decoration-underline my-2 p-3 display-3">What's happening in the world?</h2>
             {/* Mapping through array of API links from hosted Server side */}
             {items
-  .filter(item => item && item.title && item.title.toLowerCase().includes(search.toLowerCase()))
-  .map(item => (
-    <Col xs={12} md={3}>
-      <Card id='card' className='w-100 my-3 border border-3 border-dark bg-dark' key={item.urlToImage}>
-        <Card.Img variant="top" className='w-100' id='cardImg' src={item.urlToImage} alt='{item.urlToImage}' fluid/>
-          <h6 className="text-light mx-3 my-2
-          " key={item.author}><b>Author: {item.author}</b></h6>
-        <Card.Body className='text-light opacity-75'>
-          <h3 key={item.title}>{item.title}</h3>
-          <h6 key={item.description}>
-            {item.description}
-          </h6>
-          <br/>
-        </Card.Body>
-          <Button className='w-50 mx-auto my-5' variant="primary" key={item.url}><a target='_blank' rel='noopener noreferrer' className='text-decoration-none' href={item.url}>Go to Article</a></Button>
-      </Card>
-    </Col>
-  ))}
+                // .filter(item => item && item.title && item.title.toLowerCase().includes(search.toLowerCase()))
+                .map(item => (
+                  <Col xs={12} md={3}>
+                    <Card id='card' className='w-100 my-3 border border-3 border-dark bg-dark' key={item.urlToImage}>
+                      <Card.Img variant="top" className='w-100' id='cardImg' src={item.urlToImage} alt='{item.urlToImage}' fluid/>
+                        <h6 className="text-light mx-3 my-2
+                        "key={item.author}><b>Author: {item.author}</b></h6>
+                      <Card.Body className='text-light opacity-75'>
+                        <h3 key={item.title}>{item.title}</h3>
+                        <h6 key={item.description}>
+                          {item.description}
+                        </h6>
+                        <br/>
+                      </Card.Body>
+                        <Button className='w-50 mx-auto my-5' variant="primary" key={item.url}><a target='_blank' rel='noopener noreferrer' className='text-decoration-none' href={item.url}>Go to Article</a></Button>
+                    </Card>
+                  </Col>
+                ))}
 
 
           </Row>
